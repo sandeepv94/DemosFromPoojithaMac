5efb147a978113f0417bd037eea3a654f3d29d01
sowjanyappl <sowjanya@peoplelinkvc.com>
2020-01-31 14:34:18 +0530

    total code

---
+++ DemoForPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoForPush
+//
+//  Created by Peoplelink on 1/21/20.
+//  Copyright © 2020 Peoplelink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
