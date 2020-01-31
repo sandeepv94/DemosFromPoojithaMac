5e73fc221f349cf39c113ce327d66adcb3b893ff
sowjanyappl <sowjanya@peoplelinkvc.com>
2020-01-31 12:16:29 +0530

    total code

---
+++ App.js
@@ -0,0 +1,11 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Home from './Home';
+
+const MainNavigator = createStackNavigator({
+  Home:{screen: Home},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
---
+++ Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Button,Text,View, FlatList, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+export default class Home extends Component {
+    componentDidMount() {
+        console.log('sadf;aksdfjasfkl;asdlfad')
+    }
+    render () {
+        return(
+            <Text> Home </Text>
+        )
+    }
+}
